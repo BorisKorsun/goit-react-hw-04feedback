31820a87e3a67ceb0c146ce9d13ad939254a2e59
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-09-11 18:39:03 +0200

    refactoring class Component to hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import {useState} from 'react';
 
 import Section from 'components/Section';
 import FeedbackControls from 'components/FeedbackControls';
 import Statistics from 'components/Statistics';
 import Notification from 'components/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  //[1,2,3]
+export default function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  countAllFeedbacks = () => {
-    const { good, neutral, bad } = this.state;
-
+  const countAllFeedbacks = () => {
     return good + bad + neutral;
   };
 
-  countPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPercentage = () => {
     if (good === 0) {
       return 0;
     }
@@ -29,41 +23,42 @@ class App extends Component {
     return Math.round(positiveFeedbacksAverage);
   };
 
-  handleBtnClick = e => {
+  const handleBtnClick = e => {
     const { name } = e.target;
-
-    this.setState(prev => {
-      return { [name]: prev[name] + 1 };
-    });
+    
+    switch(name) {
+      case 'good': setGood(p => p + 1);
+      break;
+      case 'neutral': setNeutral(p => p + 1);
+      break;
+      case 'bad': setBad(p => p + 1);
+      break;
+      default: throw new Error(`There is no name - ${name} option`)
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackControls
-            onButtonClick={this.handleBtnClick}
-            config={['good', 'neutral', 'bad']}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackControls
+          onButtonClick={handleBtnClick}
+          config={['good', 'neutral', 'bad']}
+        />
+      </Section>
+
+      {countAllFeedbacks() > 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countAllFeedbacks()}
+            positivePercentage={countPercentage()}
           />
         </Section>
-
-        {this.countAllFeedbacks() > 0 ? (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countAllFeedbacks()}
-              positivePercentage={this.countPercentage()}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </>
-    );
-  }
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </>
+  );
 }
-
-export default App;
